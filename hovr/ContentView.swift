45d6b99d09966e5b05af3e1574fd0f0858a4ed7b
Soham Samal <sohamsamal37@gmail.com>
2026-03-15 16:21:18 +0530

    Initial Commit

---
+++ hovr/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  hovr
+//
+//  Created by Soham Samal on 15/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ hovr/hovrApp.swift
@@ -0,0 +1,17 @@
+//
+//  hovrApp.swift
+//  hovr
+//
+//  Created by Soham Samal on 15/03/26.
+//
+
+import SwiftUI
+
+@main
+struct hovrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
